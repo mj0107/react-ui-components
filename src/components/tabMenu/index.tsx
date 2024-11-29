73aa07c4cac80011a1bc1ab32679c6c59e124bf4
김민준 <alswns1285@gmail.com>
2024-11-29 18:16:17 +0900

    feat: tabMenu 구현

---
+++ src/components/tabMenu/index.tsx
@@ -0,0 +1,19 @@
+import TabMenu1 from '@/components/tabMenu/1_r';
+import TabMenu2 from '@/components/tabMenu/2_r';
+import TabMenu3 from '@/components/tabMenu/3_r';
+import TabMenu4V from '@/components/tabMenu/4_v';
+import cx from '@/components/tabMenu/cx';
+
+const TabMenus = () => {
+  return (
+    <div className={cx('TabMenus')}>
+      <h2>탭메뉴</h2>
+      <TabMenu1 />
+      <TabMenu2 />
+      <TabMenu3 />
+      <TabMenu4V />
+    </div>
+  );
+};
+
+export default TabMenus;
